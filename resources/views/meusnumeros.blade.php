47ab68ac78dd6ccc888a0ef496a5a17b0455b01b
Pedro Neres <carvalho.pedro.n@gmail.com>
2023-04-20 14:10:30 -0300

    atualizando numeros para 3

--- resources/views/meusnumeros.blade.php
+++ resources/views/meusnumeros.blade.php
@@ -1,51 +1,101 @@
-@extends('layouts.user')
-
-@section('title', 'Galdino & Filho Premiações')
-
-@section('content')
-
-                        <div class="header">
-                            <span>
-                            <ion-icon class="icon" name="cart-outline"></ion-icon> 
-                            <strong> {{Auth::user()->name}} </strong> 
-                            <small> Meus Númemros</small>
-                            </span>
-                        </div>
-
-                        @foreach(Auth::user()->compras as $compra)
-                                    <div class="body">
-                                    <div class="item-sorteio" onclick="link('/user/sorteio/show/{{$compra->sorteio->id}}')">
-                                        <img src="/img/cursos/{{$compra->sorteio->image}}" alt="{{$compra->sorteio->name}}"><br>
-                                        <div class="info-item">
-                                            <strong> {{$compra->sorteio->name}} </strong><br>
-                                            <small> {{mb_strimwidth($compra->sorteio->regulamento, 0, 50, "...")}} </small>
-                                            @if($compra->status == 1)
-                                                <div class="info-pisca2">
-                                                @if($compra->sorteio->status)
-                                                Concorrendo!
-                                                @else
-                                                Concluído 
-                                                @endif
-                                            </div>
-                                            
-                                            @else
-                                                <div class="info-pisca2 info-danger bg-danger">
-                                                    Pagamento Pendente 
-                                                    
-                                                </div>
-                                                <a href="https://wa.me/5511916853771">Mandar comprovante</a>
-                                            @endif
-                                        </div>
-                                    </div>
-                                    </div>
-                                   <div class="cotas"  onclick="link('venda/show/{{$compra->id}}')">
-                                   @foreach($compra->cotas as $cota)
-                                   @if($cota->number == $compra->sorteio->sorteado)
-                                    <div class="ct premiado">{{sprintf("%04s",$cota->number)}}</div>
-                                   @else
-                                        <div class="ct">{{sprintf("%04s",$cota->number)}}</div>
-                                    @endif
-                                    @endforeach
-                                   </div>
-                        @endforeach
+@extends('layouts.user')
+
+
+
+@section('title', 'Galdino & Filho Premiações')
+
+
+
+@section('content')
+
+
+
+                        <div class="header">
+
+                            <span>
+
+                            <ion-icon class="icon" name="cart-outline"></ion-icon> 
+
+                            <strong> {{Auth::user()->name}} </strong> 
+
+                            <small> Meus Númemros</small>
+
+                            </span>
+
+                        </div>
+
+
+
+                        @foreach(Auth::user()->compras as $compra)
+
+                                    <div class="body">
+
+                                    <div class="item-sorteio" onclick="link('/user/sorteio/show/{{$compra->sorteio->id}}')">
+
+                                        <img src="/img/cursos/{{$compra->sorteio->image}}" alt="{{$compra->sorteio->name}}"><br>
+
+                                        <div class="info-item">
+
+                                            <strong> {{$compra->sorteio->name}} </strong><br>
+
+                                            <small> {{mb_strimwidth($compra->sorteio->regulamento, 0, 50, "...")}} </small>
+
+                                            @if($compra->status == 1)
+
+                                                <div class="info-pisca2">
+
+                                                @if($compra->sorteio->status)
+
+                                                Concorrendo!
+
+                                                @else
+
+                                                Concluído 
+
+                                                @endif
+
+                                            </div>
+
+                                            
+
+                                            @else
+
+                                                <div class="info-pisca2 info-danger bg-danger">
+
+                                                    Pagamento Pendente 
+
+                                                    
+
+                                                </div>
+
+                                                <a href="https://wa.me/5511916853771">Mandar comprovante</a>
+
+                                            @endif
+
+                                        </div>
+
+                                    </div>
+
+                                    </div>
+
+                                   <div class="cotas"  onclick="link('venda/show/{{$compra->id}}')">
+
+                                   @foreach($compra->cotas as $cota)
+
+                                   @if($cota->number == $compra->sorteio->sorteado)
+
+                                    <div class="ct premiado">{{sprintf("%03s",$cota->number)}}</div>
+
+                                   @else
+
+                                        <div class="ct">{{sprintf("%04s",$cota->number)}}</div>
+
+                                    @endif
+
+                                    @endforeach
+
+                                   </div>
+
+                        @endforeach
+
 @endsection
--- resources/views/venda/comprar.blade.php
+++ resources/views/venda/comprar.blade.php
@@ -1,61 +1,121 @@
-@extends('layouts.user')
-
-@section('title', 'Galdino & Filho Premiações')
-
-@section('content')
-
-                        <div class="header">
-                            <span>
-                            <ion-icon class="icon" name="cart-outline"></ion-icon> 
-                            <strong>Compra cotas </strong> 
-                            <small> check-out</small>
-                            </span>
-                        </div>
-
-                        <div class="alert alert-info" role="alert">
-                            Você esta adquirindo {{count($venda->cotas)}} cota(s) para o sorteio <b>"{{$venda->sorteio->name}}"</b>. 
-                            Confira suas cotas abaixo e preencha seus dados.
-                        </div>
-
-                        <div class="cotas">
-                            
-                            @foreach($venda->cotas as $cota)
-                                <div class="ct">{{sprintf("%04s",$cota->number)}}</div>
-                            @endforeach
-                       
-                        </div>
-<br>
-                        <div class="form-create">
-    
-    <form action="/venda/confirmar/{{$venda->id}}" method="GET">
-        <div class="controle">
-
-        <div class="alert alert-warning" role="alert">
-                Informe seu e-mail para continuar!
-            </div>
-      
-
-            <label for="email" class="group-input">
-                <input type="email" required="required"
-                @auth value="{{Auth::user()->email}}"@endauth
-                name="email" id="email">
-                <span>Informe seu E-mail</span>
-            </label>
-            <label for="phone" class="group-input">
-                <input type="text" required="required"
-                @auth value="{{Auth::user()->phone}}"@endauth
-                name="phone" id="phone">
-                <span>Informe seu Whatsapp</span>
-            </label>
-            <div class="container-btn">
-                <button class="btn btn-primary btn-form">   
-                Continuar
-                <ion-icon class="icon" name="send"></ion-icon> 
-                </button>
-            </div>
-        </div>
-        
-    </form>
-</div>
-
+@extends('layouts.user')
+
+
+
+@section('title', 'Galdino & Filho Premiações')
+
+
+
+@section('content')
+
+
+
+                        <div class="header">
+
+                            <span>
+
+                            <ion-icon class="icon" name="cart-outline"></ion-icon> 
+
+                            <strong>Compra cotas </strong> 
+
+                            <small> check-out</small>
+
+                            </span>
+
+                        </div>
+
+
+
+                        <div class="alert alert-info" role="alert">
+
+                            Você esta adquirindo {{count($venda->cotas)}} cota(s) para o sorteio <b>"{{$venda->sorteio->name}}"</b>. 
+
+                            Confira suas cotas abaixo e preencha seus dados.
+
+                        </div>
+
+
+
+                        <div class="cotas">
+
+                            
+
+                            @foreach($venda->cotas as $cota)
+
+                                <div class="ct">{{sprintf("%03s",$cota->number)}}</div>
+
+                            @endforeach
+
+                       
+
+                        </div>
+
+<br>
+
+                        <div class="form-create">
+
+    
+
+    <form action="/venda/confirmar/{{$venda->id}}" method="GET">
+
+        <div class="controle">
+
+
+
+        <div class="alert alert-warning" role="alert">
+
+                Informe seu e-mail para continuar!
+
+            </div>
+
+      
+
+
+
+            <label for="email" class="group-input">
+
+                <input type="email" required="required"
+
+                @auth value="{{Auth::user()->email}}"@endauth
+
+                name="email" id="email">
+
+                <span>Informe seu E-mail</span>
+
+            </label>
+
+            <label for="phone" class="group-input">
+
+                <input type="text" required="required"
+
+                @auth value="{{Auth::user()->phone}}"@endauth
+
+                name="phone" id="phone">
+
+                <span>Informe seu Whatsapp</span>
+
+            </label>
+
+            <div class="container-btn">
+
+                <button class="btn btn-primary btn-form">   
+
+                Continuar
+
+                <ion-icon class="icon" name="send"></ion-icon> 
+
+                </button>
+
+            </div>
+
+        </div>
+
+        
+
+    </form>
+
+</div>
+
+
+
 @endsection
