08520b087970fff45dce97dc3cd80e7f3d81ab1c
pedro.neres <showmediapublicidade@gmail.com>
2023-05-05 23:04:06 -0300

    alterando data de limite sorteio

--- resources/views/sorteio/ganhadores.blade.php
+++ resources/views/sorteio/ganhadores.blade.php
@@ -1,29 +1,68 @@
-@extends('layouts.user')
-
-@section('title', 'Galdino & Filho Premiações')
-
-@section('content')
-
-
-<div class="header">
-                            <span>
-                            <ion-icon class="icon" name="compass"></ion-icon> 
-                            <strong> Sorteios </strong> 
-                            <small> Ganhadores</small>
-                            </span>
-                        </div>
-                        @if(count($sorteios) == 0)
-<div class="alert alert-info" role="alert">
-    Ainda não tivemos a conclusão da venda das cotas do primeiro sorteio
-</div>
-@else
-    @foreach($sorteios as $sorteio)
-            <div class="vencedor">
-            {{$sorteio->name}} <br>
-           <span> {{$sorteio->sorteado}}</span>
-           <img src="/img/sorteio1.jpeg" alt="imagem do ganhador do primeiro sorteio">
-            </div>
-    @endforeach
-@endif
-
+@extends('layouts.user')
+
+
+
+@section('title', 'Galdino & Filho Premiações')
+
+
+
+@section('content')
+
+
+
+
+
+<div class="header">
+
+                            <span>
+
+                            <ion-icon class="icon" name="compass"></ion-icon> 
+
+                            <strong> Sorteios </strong> 
+
+                            <small> Ganhadores</small>
+
+                            </span>
+
+                        </div>
+
+                        @if(count($sorteios) == 0)
+
+<div class="alert alert-info" role="alert">
+
+    Ainda não tivemos a conclusão da venda das cotas do primeiro sorteio
+
+</div>
+
+@else
+@php 
+    $cont = 0;
+@endphp
+    @foreach($sorteios as $sorteio)
+                                @php 
+                                $cont++;
+                            @endphp
+
+            <div class="vencedor">
+
+            {{$sorteio->name}} <br>
+
+           <span> {{$sorteio->sorteado}}</span>
+
+           @if($cont == 1)
+           <img src="/img/sorteio1.jpeg" alt="imagem do ganhador do primeiro sorteio">
+            @elseif($cont == 2)
+            <img src="/img/sorteio2.jpeg" alt="imagem do ganhador do primeiro sorteio">
+            @elseif($cont == 3)
+            <img src="/img/sorteio3.jpg" alt="imagem do ganhador do terceiro sorteio">
+            @endif
+
+            </div>
+
+    @endforeach
+
+@endif
+
+
+
 @endsection
