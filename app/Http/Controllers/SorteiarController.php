0ae8bba4e0410bbcbe46336333d70a92637d67ae
Pedro Neres <carvalho.pedro.n@gmail.com>
2023-06-12 23:38:14 -0300

    adicionando sorteio 10 sorteio

--- app/Http/Controllers/SorteiarController.php
+++ app/Http/Controllers/SorteiarController.php
@@ -24,7 +24,7 @@ class SorteiarController extends Controller
 
     public function pesquisar(Request $request){
 
-        $sorteio = Sorteio::findOrFail(9);
+        $sorteio = Sorteio::findOrFail(10);
 
         $vencedor = null;
 
