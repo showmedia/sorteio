dc6d554ea069ea72c3423c4742d9ba5c1bee0f43
pedro.neres <showmediapublicidade@gmail.com>
2023-06-19 14:04:28 -0300

    atualizando foto

--- app/Http/Controllers/SorteiarController.php
+++ app/Http/Controllers/SorteiarController.php
@@ -24,7 +24,7 @@ class SorteiarController extends Controller
 
     public function pesquisar(Request $request){
 
-        $sorteio = Sorteio::findOrFail(10);
+        $sorteio = Sorteio::findOrFail(11);
 
         $vencedor = null;
 
