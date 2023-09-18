4a5db2cfb531b661f02a4a77d0efe8790e9f9024
pedro.neres <showmediapublicidade@gmail.com>
2023-09-18 12:21:06 -0300

    atualizar promoção

--- resources/views/sorteio/user/show.blade.php
+++ resources/views/sorteio/user/show.blade.php
@@ -301,7 +301,7 @@
 
 
 @if($sorteio->tipo == 1)
-               <div class="desconto bg-danger info-pisca" id="info-pisca">Promoção: Pague apenas 1 real por cota na compra a partir de 20 <span id="desconto"> - *Desconto aplicado</span></div>
+               <div class="desconto bg-danger info-pisca" id="info-pisca">Promoção: Pague apenas 50 centavos por cota na compra a partir de 40 <span id="desconto"> - *Desconto aplicado</span></div>
 @endif
  
 
