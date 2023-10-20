f9a6aa33501e6547f91f1b2e882eca220651a7c7
Pedro Neres <carvalho.pedro.n@gmail.com>
2023-10-19 21:41:23 -0300

    atualizar

--- resources/views/venda/list.blade.php
+++ resources/views/venda/list.blade.php
@@ -31,7 +31,7 @@
 
 <b>Quantidade de cotas: </b>  {{$v->quantidade}} <br>
 
-<b>Cotas: </b> @foreach($v->cotas as $cota) @if($v->sorteio->tipo == 0) {{sprintf("%03s",$cota->number)}}, @else {{sprintf("%04s",$cota->number)}}, @endif @endforeach <br>
+<b>Cotas: </b> @foreach($v->cotas as $cota) @if($cota->number == 5555 || $cota->number == 9999) <span class="bg-success">@endif @if($v->sorteio->tipo == 0) {{sprintf("%03s",$cota->number)}}, @else {{sprintf("%04s",$cota->number)}}, @endif @if($cota->number == 5555 || $cota->number == 9999)  </span> @endif @endforeach <br>
 
 <b>Status: </b> @if($v->status == 0) Pendente @else Pago @endif <br>
 
