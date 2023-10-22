074b7abd47c8ba861dfd8c75594f7ce51b87e639
Pedro Neres <carvalho.pedro.n@gmail.com>
2023-10-22 11:38:43 -0300

    atualizar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -190,7 +190,7 @@
 
       <!-- SCRIPT DA APLICAÇÃO-->
 
-      <script src="/js/scriptv11.js"></script>
+      <script src="/js/scriptv12.js"></script>
 
     <!-- icons -->
 
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -332,7 +332,7 @@
 
  <!-- SCRIPT DA APLICAÇÃO-->
 
- <script src="/js/scriptv11.js"></script>
+ <script src="/js/scriptv12.js"></script>
 
    
 
